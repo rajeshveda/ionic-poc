ba28668d9886e3c823f10a5e132655116ed74062
Rajesh Narayanan <rajesh.narayanan@ieee.org>
2019-06-18 06:57:08 -0400

    Dev: pdfDemo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,11 +5,14 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 import { TabsPage } from '../pages/tabs/tabs';
 
+declare var RadaeePDFPlugin: any;
+
+
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
+  rootPage: any = TabsPage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
     platform.ready().then(() => {
@@ -17,6 +20,24 @@ export class MyApp {
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
+
+      console.log('Activate license for Radee');
+      if (platform.is('cordova')) {
+        RadaeePDFPlugin.activateLicense(
+          {
+            licenseType: 1, //0: for standard license, 1: for professional license, 2: for premium license
+            company: "IEEE", //the company name you entered during license activation
+            email: "t.glover@ieee.org", //the email you entered during license activation
+            // key: "8KIBCG-5CNBOY-Q1NDS8-ZETU10-4RHWA9-JTQ7AD" //you license activation key for ios
+            key: "LWCJNE-OEG7AF-Q1NDS8-ZETU10-0Z8ZHC-3Y4722"  //you license activation key for android
+          },
+          function (message) { // Callback for successful opening.
+            console.log("Radee Success: " + message);
+          },
+          function (err) { // Callback in case of error.
+            console.log("Radee Failure: " + err);
+          });
+      }
     });
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
+
+declare var RadaeePDFPlugin: any;
+
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,70 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public platform: Platform, public navCtrl: NavController) {
 
   }
+
+
+  public openPDF() {
+    if (this.platform.is('cordova')) {
+
+      console.log('Button clicked');
+      let filepath = 'http://www.radaeepdf.com/documentation/MRBrochoure.pdf';
+
+      this.registerCallbacks();
+      console.log('Before PDF opening');
+      let reader = new Promise((resolve, reject) => {
+        let openpdf = RadaeePDFPlugin.open(
+          {
+            url: filepath,
+            gotoPage: 0
+          },
+          (message) => {
+            console.log("Radee open pdf Success - PDF opens " + message);
+
+            // RadaeePDFPlugin.willCloseReaderCallback(this.willCloseReader());
+            //    RadaeePDFPlugin.didChangePageCallback((message) => this.updatePageNumber(message, subscription));
+          },
+          (err) => {
+            console.log("Radee open pdf Failure - PDF can't open " + err);
+          }
+        );
+        resolve(openpdf);
+      });
+
+    }
+    else
+      console.log('Not a cordova platform');
+
+  }
+
+  public registerCallbacks() {
+    RadaeePDFPlugin.willCloseReaderCallback(() => this.willCloseReader('willCloseReaderCallback'));
+
+
+    RadaeePDFPlugin.willShowReaderCallback(() => this.willCloseReader('willShowReaderCallback'));
+
+    RadaeePDFPlugin.didShowReaderCallback(() => this.willCloseReader('didShowReaderCallback'));
+
+    RadaeePDFPlugin.didCloseReaderCallback(() => this.willCloseReader('didCloseReaderCallback'));
+
+
+
+    // RadaeePDFPlugin.didSearchTermCallback(function (message) { didSearchTerm(message); });
+
+    // RadaeePDFPlugin.didTapOnPageCallback(function (message) { didTapOnPage(message); });
+
+    // RadaeePDFPlugin.didTapOnAnnotationOfTypeCallback(function (message) { didTapOnAnnotationOfType(message); });
+
+  }
+
+  public willCloseReader(funcName) {
+
+    console.log('Last seen Page', '');
+    console.log("--- Callback: ", funcName);
+
+  }
+
 
 }
